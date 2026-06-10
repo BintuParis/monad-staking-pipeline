a5c2706cc0ac0314483d0a88aec67f64337f7998
BintuParis <115546301+BintuParis@users.noreply.github.com>
2026-06-10 22:41:17 +0100

    modified the query to include self delegations

--- dailyValidatorRunningStake.sql
+++ dailyValidatorRunningStake.sql
@@ -1,4 +1,4 @@
-WITH stake_actions AS (
+WITH actions AS (
     SELECT
         validatorId AS id,
         delegator,
@@ -17,6 +17,12 @@ WITH stake_actions AS (
 
 ),
 
+-- join validator id to their validator registry to get auth address
+STAKE_ACTIONS as (
+select s.*, r.auth_address
+from actions s
+left join query_6349457 r
+on s.id = r.val_id),
 
 daily_flow AS (
 
@@ -24,13 +30,15 @@ daily_flow AS (
         date(block_time) AS daily,
         id,
         SUM(stake) AS net_total,
-        SUM(CASE WHEN delegator in (select delegator from query_7564667) THEN stake ELSE 0 END) as net_vdp
+        SUM(CASE WHEN delegator in (select delegator from query_7564667) THEN stake ELSE 0 END) as net_vdp,
+        SUM(CASE WHEN LOWER(cast(delegator as varchar)) = LOWER(auth_address) THEN stake ELSE 0 END) as NET_self_stake
     FROM stake_actions
     GROUP BY 1,2
 ),
 running_flow AS (
 select daily, id, sum(net_total) over (partition by id order by daily rows between unbounded preceding and current row) as total_stake,
- sum(net_vdp) over (partition by id order by daily rows between unbounded preceding and current row) as vdp_stake
+ sum(net_vdp) over (partition by id order by daily rows between unbounded preceding and current row) as vdp_stake,
+ SUM(net_self_stake) over (partition by id order by daily rows between unbounded preceding and current row) as self_stake
  from daily_flow
 ),
 
@@ -50,18 +58,25 @@ and date(u.timestamp) <= end_date
 ),
 
 joined AS (
-select c_date as daily, g.id, total_stake, vdp_stake
+select c_date as daily, g.id, total_stake, vdp_stake, self_stake
 from date_grid g
 left join running_flow f
 on g.c_date = f.daily
 and g.id = f.id ),
 
 bbd AS (
-select daily, id, last_value(total_stake) ignore nulls over(partition by id order by daily asc) as total_stake, last_value(vdp_stake) ignore nulls over(partition by id order by daily asc 
-rows between unbounded preceding and current row) as vdp_stake
+select daily, id, 
+last_value(total_stake) ignore nulls over(partition by id order by daily asc) as total_stake, 
+last_value(vdp_stake) ignore nulls over(partition by id order by daily asc rows between unbounded preceding and current row) as vdp_stake,
+last_value(self_stake) ignore nulls over(partition by id order by daily asc rows between unbounded preceding and current row) as self_stake
 from joined
 )
 
-select daily, id, coalesce(total_stake,0) as total_stake, coalesce(vdp_stake,0) as vdp_stake, coalesce(total_stake,0) - coalesce(vdp_stake,0) as organic_stake
+select daily, id, 
+coalesce(total_stake,0) as total_stake, 
+coalesce(vdp_stake,0) as vdp_stake, 
+coalesce(self_stake, 0) as self_stake,
+coalesce(total_stake,0) - coalesce(vdp_stake,0) as organic_stake,
+coalesce(total_stake,0) - coalesce(vdp_stake,0) - coalesce(self_stake,0) as external_stake
 from bbd
 order by daily desc, id
